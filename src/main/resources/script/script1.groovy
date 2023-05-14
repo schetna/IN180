d4bf03261694b695fb12068ef2c7382b584607ca
S0024648617 <S0024648617@techedusers.com>
2023-05-14 22:07:08 +0200

    IN180 Session Content

--- src/main/resources/script/script1.groovy
+++ src/main/resources/script/script1.groovy
@@ -2,6 +2,9 @@ import com.sap.gateway.ip.core.customdev.util.Message;
 import java.util.HashMap;
 def Message processData(Message message) {
     def body = message.getBody(java.lang.String) as String;
+    def properties = message.getProperties();
+    def productID = properties.get("ProductID");
+    body = "Sent ProductID is : " + productID
     def messageLog = messageLogFactory.getMessageLog(message);
     if(messageLog != null){
         messageLog.setStringProperty("Logging#1", "Printing Payload As Attachment")
