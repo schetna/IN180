553c1ece2dad0c03e4cf6dbc473f87996cd54a3c
S0024648617 <S0024648617@techedusers.com>
2023-05-18 01:01:49 +0200

    CICD Content

---
+++ S0024648617-flow4/src/main/resources/script/script1.groovy
@@ -0,0 +1,14 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+def Message processData(Message message) {
+    def body = message.getBody(java.lang.String) as String;
+    def properties = message.getProperties();
+    def productID = properties.get("ProductID");
+    body = "Sent ProductID is : " + productID
+    def messageLog = messageLogFactory.getMessageLog(message);
+    if(messageLog != null){
+        messageLog.setStringProperty("Logging#1", "Printing Payload As Attachment")
+        messageLog.addAttachmentAsString("ResponsePayload:", body, "text/plain");
+     }
+    return message;
+}
